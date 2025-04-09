e54340c582567ca9ad5d685965465b47208a96fa
shaheaaaad <147890169+shaheaaaad@users.noreply.github.com>
2025-04-08 20:26:53 -0400

    admin stuff

--- FinDice/urls.py
+++ FinDice/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
@@ -5,6 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('admin/', admin.site.urls),
 
     # Auth
     path('register/', views.register, name='register'),
